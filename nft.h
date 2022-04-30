5507614cce863ee92481143a54fc0f3d980643b4
MisterJaJo <jarvistrigo@gmail.com>
2022-04-30 21:05:09 +0200

    Added a cross-platform (?) way to extract only the file name from __FILE__ when outputting suite source file names

--- nft.h
+++ nft.h
@@ -11,12 +11,43 @@ extern "C" {
 /// Utility and configuration
 #define NFT_STRINGIFY(v) #v
 
+// Cross-platform way to extract the filename from the __FILE__ preprocessor macro.
+// The issue with this implementation is the duplicate call to strrchr, but we
+// can't use basename, as that one is a GNU function.
+#define __NFT_GET_FILENAME(separator) (strrchr(__FILE__, separator) ? strrchr(__FILE__, separator) + 1 : __FILE__)
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+// This is probably a Windows system
+#include <string.h>
+#define NFT_FILENAME __NFT_GET_FILENAME('\\')
+
+#elif defined(__linux__) || defined(linux) || defined(__linux)
+// This is probably a Linux system
+#include <string.h>
+#define NFT_FILENAME __NFT_GET_FILENAME('/')
+
+#elif defined(__APPLE__) || defined(__MACH__)
+// This is probably an Apple system
+#include <string.h>
+#define NFT_FILENAME __NFT_GET_FILENAME('/')
+
+#elif defined(__FreeBSD__)
+// This is probably a FreeBSD system
+#include <string.h>
+#define NFT_FILENAME __NFT_GET_FILENAME('/')
+
+#endif
+
 // The NFT_NO_STL macro disables the usage of C standard library functions.
 // This is mainly for compatibility reasons: If you don't have access to 
 // the standard library, you can provide alternative definitions to the
 // wrapper macros.
 #ifndef NFT_NO_STL
 #include <stdio.h>
+
+// TODO: find a cross-platform way to handle NFT_PRINTF without arguments
+// The issue is the following: GNU C and GNU C++ support this macro,
+// but other implementations don't do it necessarily.
 #define NFT_PRINTF(fmt, ...)          printf(fmt __VA_OPT__(,) __VA_ARGS__)
 
 #ifndef NFT_NO_STREQ
@@ -76,7 +107,7 @@ struct nft_suite_data
 // The NFT_NO_SHOW_SUITE_FILE disables this feature.
 #ifndef NFT_NO_SHOW_SUITE_FILE
 #define nft_suite(name, ...) \
-	static const char *__nft_suite_##name##_file_name = __FILE__; \
+	static const char *__nft_suite_##name##_file_name = NFT_FILENAME; \
 	void __nft_suite_##name(struct nft_suite_data *__suite_data __VA_OPT__(,) __VA_ARGS__)
 #else
 #define nft_suite(name, ...) \
